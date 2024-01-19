d232ed1d82b361da6dd21d9c4685cee5c972ad1b
JimishKhokhar <jimishkhokhar811@gmail.com>
2024-01-19 21:59:35 +0530

    Added Loading in all Dialogs

--- src/Components/Dialog.js
+++ src/Components/Dialog.js
@@ -8,6 +8,8 @@ import useMarketTimeChecker from './marketTimeChecker';
 
 const Dialog = (props) => {
 
+  const [isResultfound, setIsResultFound] = useState(0);
+
   const stock = props.stock;
   const [stockQuote, setStockQuote] = useState({});
   const [quantity, setQuantity] = useState(1);
@@ -17,7 +19,7 @@ const Dialog = (props) => {
   const [currentBalance, setCurrentBalance] = useState(-Infinity);
 
   //Market Status Checker
-  const isMarketLive=useMarketTimeChecker();
+  const isMarketLive = useMarketTimeChecker();
 
   async function findIt() {
 
@@ -63,6 +65,7 @@ const Dialog = (props) => {
 
     if (!response.ok) {
       failAlert("Not Able to Fetch Data!");
+      setIsResultFound(-1);
       return;
     }
 
@@ -74,6 +77,7 @@ const Dialog = (props) => {
 
     console.log(pureData);
     setStockQuote(pureData);
+    setIsResultFound(1)
 
     setIsDataLoading(false);
   }
@@ -81,7 +85,7 @@ const Dialog = (props) => {
   function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-  
+
 
 
 
@@ -94,105 +98,129 @@ const Dialog = (props) => {
         props.closeTheDialog();
       }}><img src={Logo} className='  w-8' ></img></button>
 
-      <div className='w-full flex flex-col align-middle items-center'>
+
+      <div className='w-full flex flex-col align-middle items-center '>
         <span className=' text-3xl font-bold text-slate-400 m-2'>{stock}</span>
-        <span className='text-3xl m-2 font-extrabold text-emerald-500 select-none '>$ {Number(stockQuote?.c).toFixed(3)}</span>
-        <div className='w-full flex  justify-around my-2 text-2xl select-none '>
-          <div><span className='font-bold'>Low: </span><span className=' p-1  bg-red-600 rounded-md'>${Number(stockQuote.l).toFixed(2)}</span></div>
-          <div><span className='font-bold'>High: </span><span className=' p-1  bg-green-600 rounded-md'>${Number(stockQuote.h).toFixed(2)}</span></div>
-        </div>
 
-        <span className='text-3xl mt-10 select-none '>Quantity</span>
-        <div className='flex gap-5 text-2xl mt-4 select-none '>
+        {
+          isResultfound == 0 ?
+            <div className='w-full min-h-[400px] flex  '>
+              <span className='m-auto text-xl'>Loading...</span>
+            </div>
+            :
+            isResultfound == -1 ?
+              <div className="w-full min-h-[400px] flex ">
+                <span className='m-auto text-xl'>Failed To Fetch Data</span>
+              </div>
+              :
+              <div className='w-full flex flex-col align-middle items-center '>
+                <span className='text-3xl m-2 font-extrabold text-emerald-500 select-none '>$ {Number(stockQuote?.c).toFixed(3)}</span>
+                <div className='w-full flex  justify-around my-2 text-2xl select-none '>
+                  <div><span className='font-bold'>Low: </span><span className=' p-1  bg-red-600 rounded-md'>${Number(stockQuote.l).toFixed(2)}</span></div>
+                  <div><span className='font-bold'>High: </span><span className=' p-1  bg-green-600 rounded-md'>${Number(stockQuote.h).toFixed(2)}</span></div>
+                </div>
 
-          {
-            quantity > 0 ? <button onClick={() => { setQuantity(quantity - 1) }} className=' text-[30px]  px-5 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span>-</span></button> :
-              <button className=' text-[30px]  px-5 bg-slate-800 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span>-</span></button>
-          }
-          {/* <span className='select-none '>{quantity}</span> */}
-          <input type='number' value={quantity} className='w-[100px] bg-black text-white text-center ' onChange={(e) => { setQuantity(Number(e.target.value)) }} />
-          <button onClick={() => { setQuantity(quantity + 1) }} className=' text-[30px]  px-4 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span className='mx-1'>+</span></button>
+                <span className='text-3xl mt-10 select-none '>Quantity</span>
+                <div className='flex gap-5 text-2xl mt-4 select-none '>
 
-
-        </div>
-        <div className='w-full flex justify-center mt-3 gap-5'>
-          <button onClick={() => { setQuantity(10) }} className=' text-[20px]  px-2 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle  '><span className='mx-1'>10</span></button>
-          <button onClick={() => { setQuantity(25) }} className=' text-[20px]  px-2 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle  '><span className='mx-1'>25</span></button>
-          <button onClick={() => { setQuantity(50) }} className=' text-[20px]  px-2 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle  '><span className='mx-1'>50</span></button>
-          <button onClick={() => { setQuantity(100) }} className=' text-[20px]  px-2 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle  '><span className='mx-1'>100</span></button>
-
-        </div>
-
-
-        <div className='absolute w-full flex flex-col  justify-center gap-5 bottom-0 select-none '>
-
-          {
-            currentBalance > (quantity * stockQuote.c) ? <span className=' self-center text-4xl font-bold text-indigo-500'>$ {Number(quantity * stockQuote.c).toFixed(3)}</span> :
-              <span className=' self-center text-4xl font-bold text-indigo-500 opacity-60'>$ {Number(quantity * stockQuote.c).toFixed(2)}</span>
-          }
+                  {
+                    quantity > 0 ? <button onClick={() => { setQuantity(quantity - 1) }} className=' text-[30px]  px-5 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span>-</span></button> :
+                      <button className=' text-[30px]  px-5 bg-slate-800 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span>-</span></button>
+                  }
+                  {/* <span className='select-none '>{quantity}</span> */}
+                  <input type='number' value={quantity} className='w-[100px] bg-black text-white text-center ' onChange={(e) => { setQuantity(Number(e.target.value)) }} />
+                  <button onClick={() => { setQuantity(quantity + 1) }} className=' text-[30px]  px-4 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span className='mx-1'>+</span></button>
 
 
+                </div>
+                <div className='w-full flex justify-center mt-3 gap-5'>
+                  <button onClick={() => { setQuantity(10) }} className=' text-[20px]  px-2 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle  '><span className='mx-1'>10</span></button>
+                  <button onClick={() => { setQuantity(25) }} className=' text-[20px]  px-2 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle  '><span className='mx-1'>25</span></button>
+                  <button onClick={() => { setQuantity(50) }} className=' text-[20px]  px-2 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle  '><span className='mx-1'>50</span></button>
+                  <button onClick={() => { setQuantity(100) }} className=' text-[20px]  px-2 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle  '><span className='mx-1'>100</span></button>
 
-          <Button
-            
-            textStyle={{ fontWeight: 'bold', fontSize: '30', fontType: 'Josefin Sans' }}
-            colorConfig={{ color: 'white', backgroundColor: '#15803D', edgeColors: { left: "black", top: 'black', right: '#D2D2D2', bottom: '#8A8A8A' } }}
-            fullWidth={true}
-            variant="secondary"
-            kind="elevated"
-            size="big"
-            colorMode="dark"
+                </div>
 
-            className='w-full  p-3 bg-green-700 text-3xl font-bold' onClick={async () => {
-              if (isDataLoading)
-                return;
-              if(!isMarketLive)
-              {
-                failAlert("Order FAILED! Market is Not Live!","top-center");
-                await sleep(3000);
-                infoAlert("Market is Active from 7:00 PM to 1:30 AM IST(Indian Standard Time) on Working days.", "top-center", 5000)
-                return;
-              }
 
-              if (quantity == 0) {
-                // alert("Quantity Must be Greater than 0");
-                failAlert("Quantity Must be Greater than 0", 'top-center');
-                return;
-              }
-              let totalPrice = Number(quantity * stockQuote.c)
-              // alert(totalPrice);
-              // alert(currentBalance)
-              // return;
-              if (currentBalance < totalPrice) {
-                failAlert(`Not Enough Balance To Trade!\nCurrent Balance: ${currentBalance}`, 'top-center');
-                // setQuantity(quantity);
-                findIt();
-                return;
-              }
+                <div className='absolute w-full flex flex-col  justify-center gap-5 bottom-0 select-none '>
 
-              let isBought = await buyStock(userObject._id, stock, stockQuote.c, quantity.toFixed(2).toString(), new Date());
-              if (isBought == -1) {
-                if (currentBalance < totalPrice) {
-                  failAlert(`Not Enough Balance To Trade!\nCurrent Balance: ${Number(currentBalance).toFixed(2)}`, 'top-center');
-                  findIt();
-                  return;
-                }
-              }
-              if (isBought == 0) {
-                //  alert("Falied to Purchase!");
-                failAlert("Falied to Purchase!", 'top-center');
-              }
-              else {
-                // alert("Successfully Bought Stock");
-                successAlert(`Successfully Bought ${stock}`, 'top-center');
-              }
-              await findIt();
-              // alert("JIMISH-"+currentBalance+"-JIMISH");
-            }}>BUY</Button>
-        </div>
+                  {
+                    currentBalance > (quantity * stockQuote.c) ? <span className=' self-center text-4xl font-bold text-indigo-500'>$ {Number(quantity * stockQuote.c).toFixed(3)}</span> :
+                      <span className=' self-center text-4xl font-bold text-indigo-500 opacity-60'>$ {Number(quantity * stockQuote.c).toFixed(2)}</span>
+                  }
+
+
+
+                  <Button
+
+                    textStyle={{ fontWeight: 'bold', fontSize: '30', fontType: 'Josefin Sans' }}
+                    colorConfig={{ color: 'white', backgroundColor: '#15803D', edgeColors: { left: "black", top: 'black', right: '#D2D2D2', bottom: '#8A8A8A' } }}
+                    fullWidth={true}
+                    variant="secondary"
+                    kind="elevated"
+                    size="big"
+                    colorMode="dark"
+
+                    className='w-full  p-3 bg-green-700 text-3xl font-bold' onClick={async () => {
+                      if (isDataLoading)
+                        return;
+                      if (!isMarketLive) {
+                        failAlert("Order FAILED! Market is Not Live!", "top-center");
+                        await sleep(3000);
+                        infoAlert("Market is Active from 7:00 PM to 1:30 AM IST(Indian Standard Time) on Working days.", "top-center", 5000)
+                        return;
+                      }
+
+                      if (quantity == 0) {
+                        // alert("Quantity Must be Greater than 0");
+                        failAlert("Quantity Must be Greater than 0", 'top-center');
+                        return;
+                      }
+                      let totalPrice = Number(quantity * stockQuote.c)
+                      // alert(totalPrice);
+                      // alert(currentBalance)
+                      // return;
+                      if (currentBalance < totalPrice) {
+                        failAlert(`Not Enough Balance To Trade!\nCurrent Balance: ${currentBalance}`, 'top-center');
+                        // setQuantity(quantity);
+                        findIt();
+                        return;
+                      }
+
+                      let isBought = await buyStock(userObject._id, stock, stockQuote.c, quantity.toFixed(2).toString(), new Date());
+                      if (isBought == -1) {
+                        if (currentBalance < totalPrice) {
+                          failAlert(`Not Enough Balance To Trade!\nCurrent Balance: ${Number(currentBalance).toFixed(2)}`, 'top-center');
+                          findIt();
+                          return;
+                        }
+                      }
+                      if (isBought == 0) {
+                        //  alert("Falied to Purchase!");
+                        failAlert("Falied to Purchase!", 'top-center');
+                      }
+                      else {
+                        // alert("Successfully Bought Stock");
+                        successAlert(`Successfully Bought ${stock}`, 'top-center');
+                      }
+                      await findIt();
+                      // alert("JIMISH-"+currentBalance+"-JIMISH");
+                    }}>BUY</Button>
+                </div>
+              </div>
+
+        }
+
+
+
+
+
+
 
 
       </div>
+
+
 
     </div>
   )
--- src/Components/sellDialog.js
+++ src/Components/sellDialog.js
@@ -12,6 +12,8 @@ const SellDialog = (props) => {
   //freeze operations untill the current operation is not completed
   const [freezeOperations, setFreezeOperations] = useState(false);
 
+  const [isResultFound, setIsResultFound] = useState(0);
+
   //Market Status Checker
   const isMarketLive = useMarketTimeChecker();
 
@@ -45,6 +47,7 @@ const SellDialog = (props) => {
 
     if (!response.ok) {
       failAlert("Not Able to Fetch Data!");
+      setIsResultFound(-1)
       return;
     }
 
@@ -52,7 +55,7 @@ const SellDialog = (props) => {
 
     console.log(pureData);
     setStockQuote(pureData);
-
+    setIsResultFound(1);
     setIsDataLoading(false);
   }
 
@@ -71,115 +74,134 @@ const SellDialog = (props) => {
 
       <div className='w-full flex flex-col align-middle items-center'>
         <span className=' text-3xl font-bold text-slate-400 m-2'>{stockToSell}</span>
-        <span className='text-3xl m-2 font-extrabold text-emerald-500 select-none '>$ {Number(stockQuote?.c).toFixed(3)}</span>
-        
 
-        <span className='text-3xl mt-10 select-none '>Quantity</span>
-        <div className='flex gap-5 text-2xl mt-4 select-none '>
-
-          {
-            quantityToSell > 0 ? <button onClick={() => { setQuantityToSell(quantityToSell - 1) }} className=' text-[30px]  px-5 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span>-</span></button> :
-              <button className=' text-[30px]  px-5 bg-slate-900 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span>-</span></button>
-          }
-          {/* <span className='select-none '>{quantity}</span> */}
-          <input type='number' value={quantityToSell} className='w-[100px] bg-black text-white text-center ' onChange={(e) => { setQuantityToSell(Number(e.target.value)) }} />
-
-          {
-            quantityToSell < maxQuantityToSell ? <button onClick={() => { setQuantityToSell(quantityToSell + 1) }} className=' text-[30px]  px-4 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span className='mx-1'>+</span></button> :
-              <button className=' text-[30px]  px-4 bg-slate-900 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span className='mx-1'>+</span></button>
-          }
-
-
-
-
-
-        </div>
-        <div className='w-full flex justify-center mt-3 gap-5'>
-          <button onClick={() => {
-            if (maxQuantityToSell >= 10)
-              setQuantityToSell(10)
-          }} className={`text-[20px] px-2  font-bold rounded-md flex justify-center align-middle ${maxQuantityToSell >= 10 ? 'bg-slate-700' : 'bg-slate-900'}   `}><span className='mx-1'>10</span></button>
-          <button onClick={() => {
-            if (maxQuantityToSell >= 25)
-              setQuantityToSell(25)
-          }} className={`text-[20px] px-2  font-bold rounded-md flex justify-center align-middle ${maxQuantityToSell >= 25 ? 'bg-slate-700' : 'bg-slate-900'}   `}><span className='mx-1'>25</span></button>
-          <button onClick={() => {
-            if (maxQuantityToSell >= 50)
-              setQuantityToSell(50)
-          }} className={`text-[20px] px-2  font-bold rounded-md flex justify-center align-middle ${maxQuantityToSell >= 50 ? 'bg-slate-700' : 'bg-slate-900'}   `}><span className='mx-1'>50</span></button>
-          <button onClick={() => {
-            if (maxQuantityToSell >= 100)
-              setQuantityToSell(100)
-          }} className={`text-[20px] px-2  font-bold rounded-md flex justify-center align-middle ${maxQuantityToSell >= 100 ? 'bg-slate-700' : 'bg-slate-900'}   `}><span className='mx-1'>100</span></button>
-
-
-
-        </div>
-        <button onClick={() => {
-          setQuantityToSell(maxQuantityToSell)
-        }} className={`mt-3 text-[20px] px-2  font-bold rounded-md flex justify-center align-middle bg-slate-700 `}><span className='mx-1'>MAX</span></button>
-
-
-
-        <div className='absolute w-full flex flex-col  justify-center gap-5 bottom-0 select-none '>
-
-          {
-            tradeTypeToExit == "B" ?
-              quantityToSell > (maxQuantityToSell) ? <span className=' self-center text-4xl font-bold text-indigo-500 opacity-60'>$ {Number(quantityToSell * stockQuote.c).toFixed(2)}</span> :
-                <span className=' self-center text-4xl font-bold text-indigo-500'>$ {Number(quantityToSell * stockQuote.c).toFixed(3)}</span>
+        {
+          isResultFound == 0 ?
+            <div className='w-full min-h-[400px] flex  '>
+              <span className='m-auto text-xl'>Loading...</span>
+            </div>
+            :
+            isResultFound == -1 ?
+              <div className="w-full min-h-[400px] flex ">
+                <span className='m-auto text-xl'>Failed To Fetch Data</span>
+              </div>
               :
-              quantityToSell > (maxQuantityToSell) ? <span className=' self-center text-4xl font-bold text-indigo-500 opacity-60'>$ {Number(2 * (Number(avgPrice) * Number(quantityToSell)) - (Number(stockQuote.c) * Number(quantityToSell))).toFixed(2)}</span> :
-                <span className=' self-center text-4xl font-bold text-indigo-500'>$ {Number(2 * (Number(avgPrice) * Number(quantityToSell)) - (Number(stockQuote.c) * Number(quantityToSell))).toFixed(2)}</span>
+              <div className='w-full flex flex-col align-middle items-center'>
+                <span className='text-3xl m-2 font-extrabold text-emerald-500 select-none '>$ {Number(stockQuote?.c).toFixed(3)}</span>
 
 
-          }
+                <span className='text-3xl mt-10 select-none '>Quantity</span>
+                <div className='flex gap-5 text-2xl mt-4 select-none '>
 
+                  {
+                    quantityToSell > 0 ? <button onClick={() => { setQuantityToSell(quantityToSell - 1) }} className=' text-[30px]  px-5 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span>-</span></button> :
+                      <button className=' text-[30px]  px-5 bg-slate-900 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span>-</span></button>
+                  }
+                  {/* <span className='select-none '>{quantity}</span> */}
+                  <input type='number' value={quantityToSell} className='w-[100px] bg-black text-white text-center ' onChange={(e) => { setQuantityToSell(Number(e.target.value)) }} />
 
-
-          <Button
-
-            textStyle={{ fontWeight: 'bold', fontSize: '30', fontType: 'Josefin Sans' }}
-            colorConfig={{ color: 'white', backgroundColor: '#DC2626', edgeColors: { left: "black", top: 'black', right: '#D2D2D2', bottom: '#8A8A8A' } }}
-            fullWidth={true}
-            variant="secondary"
-            kind="elevated"
-            size="big"
-            colorMode="dark"
-
-            className='w-full  p-3 bg-red-600 text-3xl font-bold' onClick={async () => {
-
-              // props.closeTheDialog();
-              if (freezeOperations) {
-                infoAlert("Wait To Execute The Current Order!");
-                return;
-              }
-
-              if (!isMarketLive) {
-                failAlert("Order FAILED! Market is Not Live!", "top-center");
-                await sleep(3000);
-                infoAlert("Market is Active from 7:00 PM to 1:30 AM IST(Indian Standard Time) on Working days.", "top-center", 5000)
-                return;
-              }
-
-              setFreezeOperations(true);
-
-              const result = await sellTheStock(holdingIdToSell, Number(stockQuote?.c), quantityToSell, new Date(), tradeTypeToExit);
-              console.log("back to sell Dialog with result ", result)
-              if (result == 1) {
-                successAlert(`Successfully Sold ${quantityToSell} Units of ${stockToSell}`);
-                setMaxQuantityToSell(maxQuantityToSell - quantityToSell);
-                props.updateHoldingCard(holdingIdToSell, quantityToSell);
-              }
-              else {
-                failAlert(`Not Able to Execute Order`);
-              }
-
-              setFreezeOperations(false);
+                  {
+                    quantityToSell < maxQuantityToSell ? <button onClick={() => { setQuantityToSell(quantityToSell + 1) }} className=' text-[30px]  px-4 bg-slate-700 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span className='mx-1'>+</span></button> :
+                      <button className=' text-[30px]  px-4 bg-slate-900 font-bold  rounded-md flex  justify-center  align-middle pb-2 '><span className='mx-1'>+</span></button>
+                  }
 
 
 
-            }}>EXIT</Button>
-        </div>
+
+
+                </div>
+                <div className='w-full flex justify-center mt-3 gap-5'>
+                  <button onClick={() => {
+                    if (maxQuantityToSell >= 10)
+                      setQuantityToSell(10)
+                  }} className={`text-[20px] px-2  font-bold rounded-md flex justify-center align-middle ${maxQuantityToSell >= 10 ? 'bg-slate-700' : 'bg-slate-900'}   `}><span className='mx-1'>10</span></button>
+                  <button onClick={() => {
+                    if (maxQuantityToSell >= 25)
+                      setQuantityToSell(25)
+                  }} className={`text-[20px] px-2  font-bold rounded-md flex justify-center align-middle ${maxQuantityToSell >= 25 ? 'bg-slate-700' : 'bg-slate-900'}   `}><span className='mx-1'>25</span></button>
+                  <button onClick={() => {
+                    if (maxQuantityToSell >= 50)
+                      setQuantityToSell(50)
+                  }} className={`text-[20px] px-2  font-bold rounded-md flex justify-center align-middle ${maxQuantityToSell >= 50 ? 'bg-slate-700' : 'bg-slate-900'}   `}><span className='mx-1'>50</span></button>
+                  <button onClick={() => {
+                    if (maxQuantityToSell >= 100)
+                      setQuantityToSell(100)
+                  }} className={`text-[20px] px-2  font-bold rounded-md flex justify-center align-middle ${maxQuantityToSell >= 100 ? 'bg-slate-700' : 'bg-slate-900'}   `}><span className='mx-1'>100</span></button>
+
+
+
+                </div>
+                <button onClick={() => {
+                  setQuantityToSell(maxQuantityToSell)
+                }} className={`mt-3 text-[20px] px-2  font-bold rounded-md flex justify-center align-middle bg-slate-700 `}><span className='mx-1'>MAX</span></button>
+
+
+
+                <div className='absolute w-full flex flex-col  justify-center gap-5 bottom-0 select-none '>
+
+                  {
+                    tradeTypeToExit == "B" ?
+                      quantityToSell > (maxQuantityToSell) ? <span className=' self-center text-4xl font-bold text-indigo-500 opacity-60'>$ {Number(quantityToSell * stockQuote.c).toFixed(2)}</span> :
+                        <span className=' self-center text-4xl font-bold text-indigo-500'>$ {Number(quantityToSell * stockQuote.c).toFixed(3)}</span>
+                      :
+                      quantityToSell > (maxQuantityToSell) ? <span className=' self-center text-4xl font-bold text-indigo-500 opacity-60'>$ {Number(2 * (Number(avgPrice) * Number(quantityToSell)) - (Number(stockQuote.c) * Number(quantityToSell))).toFixed(2)}</span> :
+                        <span className=' self-center text-4xl font-bold text-indigo-500'>$ {Number(2 * (Number(avgPrice) * Number(quantityToSell)) - (Number(stockQuote.c) * Number(quantityToSell))).toFixed(2)}</span>
+
+
+                  }
+
+
+
+                  <Button
+
+                    textStyle={{ fontWeight: 'bold', fontSize: '30', fontType: 'Josefin Sans' }}
+                    colorConfig={{ color: 'white', backgroundColor: '#DC2626', edgeColors: { left: "black", top: 'black', right: '#D2D2D2', bottom: '#8A8A8A' } }}
+                    fullWidth={true}
+                    variant="secondary"
+                    kind="elevated"
+                    size="big"
+                    colorMode="dark"
+
+                    className='w-full  p-3 bg-red-600 text-3xl font-bold' onClick={async () => {
+
+                      // props.closeTheDialog();
+                      if (freezeOperations) {
+                        infoAlert("Wait To Execute The Current Order!");
+                        return;
+                      }
+
+                      if (!isMarketLive) {
+                        failAlert("Order FAILED! Market is Not Live!", "top-center");
+                        await sleep(3000);
+                        infoAlert("Market is Active from 7:00 PM to 1:30 AM IST(Indian Standard Time) on Working days.", "top-center", 5000)
+                        return;
+                      }
+
+                      setFreezeOperations(true);
+
+                      const result = await sellTheStock(holdingIdToSell, Number(stockQuote?.c), quantityToSell, new Date(), tradeTypeToExit);
+                      console.log("back to sell Dialog with result ", result)
+                      if (result == 1) {
+                        successAlert(`Successfully Sold ${quantityToSell} Units of ${stockToSell}`);
+                        setMaxQuantityToSell(maxQuantityToSell - quantityToSell);
+                        props.updateHoldingCard(holdingIdToSell, quantityToSell);
+                      }
+                      else {
+                        failAlert(`Not Able to Execute Order`);
+                      }
+
+                      setFreezeOperations(false);
+
+
+
+                    }}>EXIT</Button>
+                </div>
+
+              </div>
+        }
+
+
+
 
 
       </div>
